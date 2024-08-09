2ee025940a0adefa42fe82d2b0728d78bb4bfad1
sozerong <dudfo0893@naver.com>
2024-08-09 17:47:15 +0900

    'deploy'

--- smumarket/settings.py
+++ smumarket/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url 
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
